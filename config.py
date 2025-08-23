0aecedd689688be465ca4990f4fb9b60b697c64c
deweni123456789 <schoford91@gmail.com>
2025-08-23 20:50:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "5047271"))
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8400227620:AAEje87350ILP5lJxMeb57rOn9K8yBY1f2o")
 DEVELOPER = os.getenv("DEVELOPER", "@deweni2")
