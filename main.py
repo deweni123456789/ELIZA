ecac80ab7835f55438b425e9c6dd91f78f0937b4
deweni123456789 <schoford91@gmail.com>
2025-08-24 22:42:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config
 from modules.tiktok import register as register_tiktok
-from modules.instagram import register as register_instagram
 from modules.song import register as register_song
 from modules.video import register as register_video
 
@@ -36,14 +35,6 @@ register_song(app)
 register_video(app)
 register_tiktok(app)
 
-# --- FIXED: make register_instagram synchronous registration
-def register_instagram_sync(app):
-    import asyncio
-    from modules.instagram import register as register_instagram_async
-    # run the async register function safely
-    asyncio.get_event_loop().run_until_complete(register_instagram_async(app))
-
-register_instagram(app)
 
 # ---- Run bot (Pyrogram v2 style)
 app.run()
