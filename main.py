e64642d74fc70ecf04b4eb59e52780fd3c62196b
deweni123456789 <schoford91@gmail.com>
2025-08-27 10:58:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config
 from modules.tiktok import register as register_tiktok, handle_callbacks as tiktok_callbacks
+from modules.adult_downloader import register as register_adult_downloader
 from modules.song import register as register_song
 from modules.video import register as register_video
 
@@ -35,6 +36,7 @@ async def callbacks(bot, query):
 register_song(app)
 register_video(app)
 register_tiktok(app)
+register_adult_downloader(app)
 
 # ---- Run bot (Pyrogram v2 style)
 app.run()
